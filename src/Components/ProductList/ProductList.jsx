e9cb21a5b89c48b0ef152cefe36b8aa06a82e6db
Abhinav <you@example.com>
2025-05-20 17:15:18 +0530

    unlocked the filds

--- src/Components/ProductList/ProductList.jsx
+++ src/Components/ProductList/ProductList.jsx
@@ -56,34 +56,51 @@ const ProductList = () => {
       <div className="price-list-container">
         <div className="table-scroll-wrapper">
           <table className="price-list-table">
-            <thead>
-              <tr>
-                <th></th>
-                <th className='arrow-down'>Article No.</th>
-                <th className='arrow-down'>Product/Service</th>
-                <th>In Price</th>
-                <th>Price</th>
-                <th>Unit</th>
-                <th>In Stock</th>
-                <th>Description</th>
-                <th>Options</th>
-              </tr>
-            </thead>
-            <tbody>
-              {products.map((product) => (
-                <tr key={product.id}>
-                  <td className="arrow-right"></td>
-                  <td><input value={product.article_no} onChange={(e) => handleInputChange(product.id, 'article_no', e.target.value)} /></td>
-                  <td><input value={product.product_name} onChange={(e) => handleInputChange(product.id, 'product_name', e.target.value)} /></td>
-                  <td><input value={product.in_price} onChange={(e) => handleInputChange(product.id, 'in_price', e.target.value)} /></td>
-                  <td><input value={product.price} onChange={(e) => handleInputChange(product.id, 'price', e.target.value)} /></td>
-                  <td><input value={product.unit} onChange={(e) => handleInputChange(product.id, 'unit', e.target.value)} /></td>
-                  <td><input value={product.in_stock} onChange={(e) => handleInputChange(product.id, 'in_stock', e.target.value)} /></td>
-                  <td><input value={product.description} onChange={(e) => handleInputChange(product.id, 'description', e.target.value)} /></td>
-                  <td><button className='save-button' onClick={() => handleSave(product.id)}>Save</button></td>
-                </tr>
-              ))}
-            </tbody>
+          <thead>
+  <tr>
+    <th className="column-arrow"></th>
+    <th className="column-article-no arrow-down">Article No.</th>
+    <th className="column-name arrow-down">Product/Service</th>
+    <th className="column-in-price">In Price</th>
+    <th className="column-price">Price</th>
+    <th className="column-unit">Unit</th>
+    <th className="column-stock">In Stock</th>
+    <th className="column-description">Description</th>
+    <th className="column-options">Options</th>
+  </tr>
+</thead>
+<tbody>
+  {products.map((product) => (
+    <tr key={product.id}>
+      <td className="column-arrow arrow-right"></td>
+      <td className="column-article-no">
+        <input value={product.article_no} onChange={(e) => handleInputChange(product.id, 'article_no', e.target.value)} />
+      </td>
+      <td className="column-name">
+        <input value={product.product_name} onChange={(e) => handleInputChange(product.id, 'product_name', e.target.value)} />
+      </td>
+      <td className="column-in-price">
+        <input value={product.in_price} onChange={(e) => handleInputChange(product.id, 'in_price', e.target.value)} />
+      </td>
+      <td className="column-price">
+        <input value={product.price} onChange={(e) => handleInputChange(product.id, 'price', e.target.value)} />
+      </td>
+      <td className="column-unit">
+        <input value={product.unit} onChange={(e) => handleInputChange(product.id, 'unit', e.target.value)} />
+      </td>
+      <td className="column-stock">
+        <input value={product.in_stock} onChange={(e) => handleInputChange(product.id, 'in_stock', e.target.value)} />
+      </td>
+      <td className="column-description">
+        <input value={product.description} onChange={(e) => handleInputChange(product.id, 'description', e.target.value)} />
+      </td>
+      <td className="column-options">
+        <button className="save-button" onClick={() => handleSave(product.id)}>Save</button>
+      </td>
+    </tr>
+  ))}
+</tbody>
+
           </table>
         </div>
       </div>
